5de931a6092ed47a4545af482179d59d39cb81f6
Ankitatiwary21 <125357910+Ankitatiwary21@users.noreply.github.com>
2026-06-29 14:03:06 +0530

    Fix MongoDB connection string in db.jsremoved pass
    Updated MongoDB connection string format.

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(
-            'mongodb+srv://Anku:Anku@cluster0.32nl656.mongodb.net/YTMongoTest?retryWrites=true&w=majority&appName=Cluster0'
+         
         );
 
         console.log('MongoDB connected...');
